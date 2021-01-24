bcfd4ddc32d7f406ccf9b6d2546dafe271f7341e
hakansarac <hakan.sarac95@gmail.com>
2021-01-24 20:11:51 +0300

    checking location is enabled or disabled added

--- app/src/main/java/com/hakansarac/weatherapp/MainActivity.kt
+++ app/src/main/java/com/hakansarac/weatherapp/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.hakansarac.weatherapp
 
+import android.content.Context
+import android.content.Intent
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
+import android.provider.Settings
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (!isLocationEnabled()) {
+            Toast.makeText(
+                this,
+                "Your location provider is turned off. Please turn it on.",
+                Toast.LENGTH_SHORT
+            ).show()
+            val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            startActivity(intent)
+        }else {
+            //TODO: add what will do
+        }
+
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager =
+            getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
     }
 }
